bcb1c33dee71333afc424c8fefd8a34381d9ab19
Sahit N <63145693+LmaoPewPew@users.noreply.github.com>
2023-11-01 18:03:17 +0100

    Add files via upload

---
+++ SoftProject/src/main/java/com/softpro/dnaig/MainApplication.java
@@ -0,0 +1,24 @@
+package com.softpro.dnaig;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainApplication extends Application {
+    final double screenRes = 1331.2;
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("DNAIG.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), screenRes, screenRes/16*9);
+        stage.setTitle("DNAIG-RayTracer");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
