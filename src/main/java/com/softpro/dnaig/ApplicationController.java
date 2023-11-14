4b98e894b98437ba6d1f5992b3a7d0a1ef61915a
LmaoPewPew <asdfghjkl.98@gmx.de>
2023-11-14 20:59:46 +0100

    objectProperties

--- src/main/java/com/softpro/dnaig/ApplicationController.java
+++ src/main/java/com/softpro/dnaig/ApplicationController.java
@@ -1,7 +1,7 @@
 package com.softpro.dnaig;
 
 import com.softpro.dnaig.objData.Entity;
-import com.softpro.dnaig.preview.PreviewWindow;
+import com.softpro.dnaig.object.ObjectProperties;
 import com.softpro.dnaig.utils.ObjFileReader;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
@@ -35,6 +35,8 @@ public class ApplicationController {
 
     @FXML
     private SubScene subScenePreview;
+    private List<ObjectProperties> propertiesList = new ArrayList<>();
+
 
     private final FileChooser fileChooser = new FileChooser();
 
@@ -45,7 +47,7 @@ public class ApplicationController {
     @FXML
     void importLightObject(MouseEvent event) {
         System.out.println("Testst");
-        showObjects(1);
+        //showObjects(1);
     }
 
     @FXML
@@ -57,29 +59,47 @@ public class ApplicationController {
             Entity entity = ObjFileReader.createObject(loadObjectFilePath.getPath());
             System.out.println(entity);
             entityList.add(entity);
-            Runtime.getRuntime().exec(new String[]{"C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge", "https://youtu.be/dQw4w9WgXcQ"});
             coordTextField.setText(entity.toString());
 
-            showObjects(0);
+            showObjects(entity, 0);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    void showObjects(int i) {
+    void showObjects(Entity e, int i) {
+        //PreView Window
         loadPreviewer();
 
+        //ObjectList Region
         objectListView.setPadding(new Insets(10, 5, 10, 5));
-        if (i == 0)
-            objectListView.getItems().add(new ImageView("D:\\Code\\java\\school\\sofo\\SoftProject\\src\\main\\resources\\com\\softpro\\dnaig\\sprites\\obj_Img_LIGHTTHEME.png"));
-        else if (i == 1)
-            objectListView.getItems().add(new ImageView("D:\\Code\\java\\school\\sofo\\SoftProject\\src\\main\\resources\\com\\softpro\\dnaig\\sprites\\light_Img_LIGHTTHEME.png"));
-        else System.out.println("n/a");
+        if (i == 0) {
+            fillObjectProperties(e, i);
+            objectListView.getItems().add(propertiesList.get(0).getImageView()); //Nix gehen
+        } else if (i == 1) {
+            objectListView.getItems().add(new ImageView("D:\\Code\\java\\school\\sofo\\src\\main\\resources\\com\\softpro\\dnaig\\sprites\\light_Img_LIGHTTHEME.png"));
+            fillObjectProperties(i);
+        } else System.out.println("n/a");
+
+
+        //TODO:: Set Fix Image View SIZE!!!!
+
+    }
+
+    void fillObjectProperties(Entity e, int id) {
+        ObjectProperties op = new ObjectProperties(e.getObjName(), Integer.toString(e.getID()), Integer.toString(e.getFaces().size()), Integer.toString(e.getVertexCount()), new String[]{Float.toString(e.getPivot().getX()), Float.toString(e.getPivot().getY()), Float.toString(e.getPivot().getZ())}, new String[]{Float.toString(e.getOrient().getX()), Float.toString(e.getOrient().getY()), Float.toString(e.getOrient().getZ())});
+
+        op.setImageView(new ImageView("D:\\Code\\java\\school\\sofo\\src\\main\\resources\\com\\softpro\\dnaig\\sprites\\obj_Img_LIGHTTHEME.png"));
+        propertiesList.add(op);
+    }
+
+    void fillObjectProperties(int id) {
+
 
     }
 
     void loadPreviewer() {
-        PreviewWindow pw = new PreviewWindow(subScenePreview);
+        //TODO: Preview Window
     }
 
 
@@ -89,6 +109,6 @@ public class ApplicationController {
 
     }
 
-    //initializer, get last used filepath
+    //TODO: initializer, get last used filepath
 
 }
---
+++ src/main/java/com/softpro/dnaig/object/ObjectProperties.java
@@ -0,0 +1,92 @@
+package com.softpro.dnaig.object;
+
+import javafx.scene.image.ImageView;
+
+import java.util.Arrays;
+
+public class ObjectProperties {
+    //Object name: object0
+    // ID: 0
+    // Faces: 8207
+    // Vertices: 11312
+    // Position: x: 0,000000 y: 0,000000 z: 0,000000
+    private String objName;
+    private String objID;
+    private String objFaces;
+    private String objVertices;
+    private String objPos[];
+    private String objRot[];
+    private ImageView imageView;
+
+
+    public String getObjID() {
+        return objID;
+    }
+
+    public void setObjID(String objID) {
+        this.objID = objID;
+    }
+
+    public String getObjName() {
+        return objName;
+    }
+
+    public void setObjName(String objName) {
+        this.objName = objName;
+    }
+
+    public String getObjFaces() {
+        return objFaces;
+    }
+
+    public void setObjFaces(String objFaces) {
+        this.objFaces = objFaces;
+    }
+
+    public String getObjVertices() {
+        return objVertices;
+    }
+
+    public void setObjVertices(String objVertices) {
+        this.objVertices = objVertices;
+    }
+
+    public String[] getObjPos() {
+        return objPos;
+    }
+
+    public void setObjPos(String[] objPos) {
+        this.objPos = objPos;
+    }
+
+    public String[] getObjRot() {
+        return objRot;
+    }
+
+    public void setObjRot(String[] objRot) {
+        this.objRot = objRot;
+    }
+
+    public ImageView getImageView() {
+        return imageView;
+    }
+
+    public void setImageView(ImageView imageView) {
+        this.imageView = imageView;
+    }
+
+    public ObjectProperties(String objID, String objName, String objFaces, String objVertices, String[] objPos, String[] objRot) {
+        this.objID = objID;
+        this.objName = objName;
+        this.objFaces = objFaces;
+        this.objVertices = objVertices;
+        this.objPos = objPos;
+        this.objRot = objRot;
+    }
+
+
+    @Override
+    public String toString() {
+        return "ObjectProperties{" + "objID='" + objID + '\'' + ", objName='" + objName + '\'' + ", objFaces='" + objFaces + '\'' + ", objVertices='" + objVertices + '\'' + ", objPos=" + Arrays.toString(objPos) + ", objRot=" + Arrays.toString(objRot) + '}';
+    }
+}
