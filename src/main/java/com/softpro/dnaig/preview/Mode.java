d493071691ed648c07befd56eb0e532e6a938372
Dominik Bamberger <dominikbamberger@gmail.com>
2023-11-25 01:03:55 +0100

    Preview: fix formatting

--- src/main/java/com/softpro/dnaig/preview/Mode.java
+++ src/main/java/com/softpro/dnaig/preview/Mode.java
@@ -2,7 +2,23 @@ package com.softpro.dnaig.preview;
 
 public enum Mode {
 
-    ERROR("01100101 01110010 01110010 01101111 01110010", TargetType.ERROR, ActionType.ERROR), MOVE_CAMERA_XY("move camera (xy)", TargetType.CAMERA, ActionType.MOVE), MOVE_CAMERA_X("move camera (x)", TargetType.CAMERA, ActionType.MOVE), MOVE_CAMERA_Y("move camera (y)", TargetType.CAMERA, ActionType.MOVE), MOVE_CAMERA_Z("move camera (z)", TargetType.CAMERA, ActionType.MOVE), MOVE_OBJECT_XY("move object (xy)", TargetType.OBJECT, ActionType.MOVE), MOVE_OBJECT_X("move object (x)", TargetType.OBJECT, ActionType.MOVE), MOVE_OBJECT_Y("move object (y)", TargetType.OBJECT, ActionType.MOVE), MOVE_OBJECT_Z("move object (z)", TargetType.OBJECT, ActionType.MOVE), ROTATE_CAMERA_XY("rotate camera (xy)", TargetType.CAMERA, ActionType.ROTATE), ROTATE_CAMERA_X("rotate camera (x)", TargetType.CAMERA, ActionType.ROTATE), ROTATE_CAMERA_Y("rotate camera (y)", TargetType.CAMERA, ActionType.ROTATE), ROTATE_CAMERA_Z("rotate camera (z)", TargetType.CAMERA, ActionType.ROTATE), ROTATE_OBJECT_XY("rotate object (xy)", TargetType.OBJECT, ActionType.ROTATE), ROTATE_OBJECT_X("rotate object (x)", TargetType.OBJECT, ActionType.ROTATE), ROTATE_OBJECT_Y("rotate object (y)", TargetType.OBJECT, ActionType.ROTATE), ROTATE_OBJECT_Z("rotate object (z)", TargetType.OBJECT, ActionType.ROTATE),
+    ERROR("01100101 01110010 01110010 01101111 01110010", TargetType.ERROR, ActionType.ERROR),
+    MOVE_CAMERA_XY("move camera (xy)", TargetType.CAMERA, ActionType.MOVE),
+    MOVE_CAMERA_X("move camera (x)", TargetType.CAMERA, ActionType.MOVE),
+    MOVE_CAMERA_Y("move camera (y)", TargetType.CAMERA, ActionType.MOVE),
+    MOVE_CAMERA_Z("move camera (z)", TargetType.CAMERA, ActionType.MOVE),
+    MOVE_OBJECT_XY("move object (xy)", TargetType.OBJECT, ActionType.MOVE),
+    MOVE_OBJECT_X("move object (x)", TargetType.OBJECT, ActionType.MOVE),
+    MOVE_OBJECT_Y("move object (y)", TargetType.OBJECT, ActionType.MOVE),
+    MOVE_OBJECT_Z("move object (z)", TargetType.OBJECT, ActionType.MOVE),
+    ROTATE_CAMERA_XY("rotate camera (xy)", TargetType.CAMERA, ActionType.ROTATE),
+    ROTATE_CAMERA_X("rotate camera (x)", TargetType.CAMERA, ActionType.ROTATE),
+    ROTATE_CAMERA_Y("rotate camera (y)", TargetType.CAMERA, ActionType.ROTATE),
+    ROTATE_CAMERA_Z("rotate camera (z)", TargetType.CAMERA, ActionType.ROTATE),
+    ROTATE_OBJECT_XY("rotate object (xy)", TargetType.OBJECT, ActionType.ROTATE),
+    ROTATE_OBJECT_X("rotate object (x)", TargetType.OBJECT, ActionType.ROTATE),
+    ROTATE_OBJECT_Y("rotate object (y)", TargetType.OBJECT, ActionType.ROTATE),
+    ROTATE_OBJECT_Z("rotate object (z)", TargetType.OBJECT, ActionType.ROTATE),
     ;
 
     private final String name;
@@ -25,10 +41,14 @@ public enum Mode {
     }
 
     protected enum TargetType {
-        CAMERA, OBJECT, ERROR
+        CAMERA,
+        OBJECT,
+        ERROR
     }
 
     protected enum ActionType {
-        MOVE, ROTATE, ERROR
+        MOVE,
+        ROTATE,
+        ERROR
     }
 }
